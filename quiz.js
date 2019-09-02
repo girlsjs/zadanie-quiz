eaa1d0faffdf8b4a89ff573077b1e8a204e1f48d
ewcia007 <ewa.swiatczynska@gmail.com>
2019-09-02 21:58:16 +0200

    add showNextQuestion function

--- quiz.js
+++ quiz.js
@@ -39,7 +39,25 @@ const questions = [
     }
   ];
 
+  // show question
   function showQuestion(i) {
     var paragraph = document.querySelector('main p');
     paragraph.textContent = questions[i].question;
   }
+
+  // show next question
+  const nextButton = document.getElementById('next-btn');
+  nextButton.addEventListener("click", showNextQuestion);
+  var currentQuestion = i;
+  
+  function showNextQuestion() {
+    showQuestion(currentQuestion + 1);
+    if (currentQuestion === questions.length - 1) {
+      nextButton.style.display = 'none';
+    } else {
+      nextButton.style.display = 'block';
+    }
+  }
+
+
+  
