8a3c9b326e38af9bf2afe59f05c22d3d2a0c61eb
evelynn2727 <totalnieniewybaczalne.ep@gmail.com>
2019-09-07 18:32:48 +0200

    progress bar

--- quiz.js
+++ quiz.js
@@ -43,6 +43,7 @@ const questions = [
   function showQuestion(i) {
     let paragraph = document.querySelector('main p');
     paragraph.textContent = questions[i].question;
+
   }
 
   // show next question
@@ -55,6 +56,7 @@ const questions = [
     currentQuestion += 1;
     if (currentQuestion === questions.length - 1) {
       nextButton.style.display = 'none';
+
     } else {
       nextButton.style.display = 'block';
     }
@@ -72,3 +74,47 @@ const questions = [
       <p>${contentText}</p>
     `;
   }
+
+  //INFORMATION PANEL
+let step = 'step1';
+
+const step1 = document.getElementById('step1');
+const step2 = document.getElementById('step2');
+const step3 = document.getElementById('step3');
+const step4 = document.getElementById('step4');
+
+function next() {
+  if (step === 'step1') {
+    step = 'step2';
+    step1.classList.remove("is-active");
+    step1.classList.add("is-complete");
+    step2.classList.add("is-active");
+
+  } else if (step === 'step2') {
+    step = 'step3';
+    step2.classList.remove("is-active");
+    step2.classList.add("is-complete");
+    step3.classList.add("is-active");
+
+  } else if (step === 'step3') {
+    step = 'step4';
+    step3.classList.remove("is-active");
+    step3.classList.add("is-complete");
+    step4.classList.add("is-active");
+
+  } else if (step === 'step4') {
+    step = 'complete';
+    step4.classList.remove("is-active");
+    step4.classList.add("is-complete");
+
+  } else if (step === 'complete') {
+    step = 'step1';
+    step4.classList.remove("is-complete");
+    step3.classList.remove("is-complete");
+    step2.classList.remove("is-complete");
+    step1.classList.remove("is-complete");
+
+    step1.classList.add("is-active");
+  }
+}
+//END INFORMATION PANEL
