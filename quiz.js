b8609dbadedbe69ad6d0b8980733b79e8e8528dd
Daria Puchnarewicz <Daria.Puchnarewicz@stepstone.com>
2019-09-11 12:36:43 +0200

    change button class to btn

--- quiz.js
+++ quiz.js
@@ -1,108 +1,108 @@
 const questions = [
-    {
-      question: "Nazwa zmiennej nie może zaczynać się od:",
-      answers: {
-        a: "dolara",
-        b: "cyfry",
-        c: "dużej litery"
-      },
-      correctAnswer: "b"
+  {
+    question: "Nazwa zmiennej nie może zaczynać się od:",
+    answers: {
+      a: "dolara",
+      b: "cyfry",
+      c: "dużej litery"
     },
-    {
-      question:
-        "function square(number) {\r\n return number * number; \r\n } \r\nto przykład:",
-      answers: {
-        a: "deklaracji funkcji",
-        b: "wywołania funkcji",
-        c: "domknięcia funkcji"
-      },
-      correctAnswer: "a"
+    correctAnswer: "b"
+  },
+  {
+    question:
+      "function square(number) {\r\n return number * number; \r\n } \r\nto przykład:",
+    answers: {
+      a: "deklaracji funkcji",
+      b: "wywołania funkcji",
+      c: "domknięcia funkcji"
     },
-    {
-      question:
-        "Który operator porównuje dwa operandy z uwzględnieniem ich typu?",
-      answers: {
-        a: "==",
-        b: "!=",
-        c: "==="
-      },
-      correctAnswer: "c"
+    correctAnswer: "a"
+  },
+  {
+    question:
+      "Który operator porównuje dwa operandy z uwzględnieniem ich typu?",
+    answers: {
+      a: "==",
+      b: "!=",
+      c: "==="
     },
-    {
-      question: "Inna nazwa języka JavaScript to:",
-      answers: {
-        a: "ActionScript",
-        b: "Java",
-        c: "ECMAScript"
-      },
-      correctAnswer: "c"
-    }
-  ];
+    correctAnswer: "c"
+  },
+  {
+    question: "Inna nazwa języka JavaScript to:",
+    answers: {
+      a: "ActionScript",
+      b: "Java",
+      c: "ECMAScript"
+    },
+    correctAnswer: "c"
+  }
+];
 
-  // show question
-  function showQuestion(i) {
-    let paragraph = document.querySelector('main p');
-    paragraph.textContent = questions[i].question;
+// show question
+function showQuestion(i) {
+  let paragraph = document.querySelector('main p');
+  paragraph.textContent = questions[i].question;
+}
+
+// show next question
+const nextButton = document.getElementById('next-btn');
+nextButton.addEventListener("click", showNextQuestion);
+let currentQuestion = 0;
+
+const resultButton = document.getElementById('result-btn');
+resultButton.addEventListener("click", showResult);
+resultButton.style.display = 'none';
+
+
+function showNextQuestion() {
+  showQuestion(currentQuestion + 1);
+  currentQuestion += 1;
+  if (currentQuestion === questions.length - 1) {
+    nextButton.style.display = 'none';
+
+    resultButton.style.display = 'block';
+    showResult();
+  } else {
+    nextButton.style.display = 'block';
   }
 
-  // show next question
-  const nextButton = document.getElementById('next-btn');
-  nextButton.addEventListener("click", showNextQuestion);
-  let currentQuestion = 0;
+}
 
-  const resultButton = document.getElementById('result-btn');
-  resultButton.addEventListener("click", showResult);
-  resultButton.style.display = 'none';
 
-  
-  function showNextQuestion() {
-    showQuestion(currentQuestion + 1);
-    currentQuestion += 1;
-    if (currentQuestion === questions.length - 1) {
-      nextButton.style.display = 'none';
+function showResult() {
 
-      resultButton.style.display = 'block';
-      showResult();
-    } else {
-      nextButton.style.display = 'block';
-    }
-    
-  }
+  let correctAnswers = 3;
+  let titleText = 'Koniec Quizu!';
+  let contentText = `Odpowiedziałaś dobrze na ${correctAnswers} z ${questions.length} pytań.`;
 
-  
-  function showResult() {
-
-    let correctAnswers = 3;
-    let titleText = 'Koniec Quizu!';
-    let contentText = `Odpowiedziałaś dobrze na ${correctAnswers} z ${questions.length} pytań.`;
-
-    const mainElement = document.querySelector('main');
-    mainElement.innerHTML= `
+  const mainElement = document.querySelector('main');
+  mainElement.innerHTML = `
       <p>${titleText}</p>
       <p>${contentText}</p>
-      <button id="last-btn" class="last">Jeszcze raz</button>
+      <button id="last-btn" class="btn">Jeszcze raz</button>
     `;
-    const lastButton = document.getElementById('last-btn');
-    lastButton.addEventListener('click', startQuiz);
+  const lastButton = document.getElementById('last-btn');
+  lastButton.addEventListener('click', startQuiz);
 
-  }
+}
 
-  function startQuiz(){
-     const mainElement = document.querySelector('main');
-    mainElement.innerHTML= `
+function startQuiz() {
+  const mainElement = document.querySelector('main');
+  mainElement.innerHTML = `
     <p>treść pytania</p>
     <radiogroup class="answers">         
     </radiogroup>
-    <button id="next-btn" class="next">Dalej</button>
+    <button id="next-btn" class="btn">Dalej</button>
     `
-    const nextButton = document.getElementById('next-btn');
-    nextButton.addEventListener("click", showNextQuestion);
-      
-    currentQuestion= 0; 
-    showQuestion(currentQuestion);
-    ;
-  }
+  const nextButton = document.getElementById('next-btn');
+  nextButton.addEventListener("click", showNextQuestion);
 
-  startQuiz();
+  currentQuestion = 0;
+  showQuestion(currentQuestion);
+  ;
+}
+
+startQuiz();
 
 
