a4eb1c4df61864c3f3a6edf4330b3966914b8911
evelynn2727 <totalnieniewybaczalne.ep@gmail.com>
2019-09-21 08:43:14 +0200

    new changes in projects

--- quiz.js
+++ quiz.js
@@ -1,79 +1,166 @@
-const questions = [
-    {
-      question: "Nazwa zmiennej nie może zaczynać się od:",
-      answers: {
-        a: "dolara",
-        b: "cyfry",
-        c: "dużej litery"
-      },
-      correctAnswer: "b"
+const questions = [{
+  question: "Nazwa zmiennej nie może zaczynać się od:",
+  answers: {
+    a: "dolara",
+    b: "cyfry",
+    c: "dużej litery"
+  },
+  correctAnswer: "b"
+},
+  {
+    question: "function square(number) {\r\n return number * number; \r\n } \r\nto przykład:",
+    answers: {
+      a: "deklaracji funkcji",
+      b: "wywołania funkcji",
+      c: "domknięcia funkcji"
     },
-    {
-      question:
-        "function square(number) {\r\n return number * number; \r\n } \r\nto przykład:",
-      answers: {
-        a: "deklaracji funkcji",
-        b: "wywołania funkcji",
-        c: "domknięcia funkcji"
-      },
-      correctAnswer: "a"
+    correctAnswer: "a"
+  },
+  {
+    question: "Który operator porównuje dwa operandy z uwzględnieniem ich typu?",
+    answers: {
+      a: "==",
+      b: "!=",
+      c: "==="
     },
-    {
-      question:
-        "Który operator porównuje dwa operandy z uwzględnieniem ich typu?",
-      answers: {
-        a: "==",
-        b: "!=",
-        c: "==="
-      },
-      correctAnswer: "c"
+    correctAnswer: "c"
+  },
+  {
+    question: "Inna nazwa języka JavaScript to:",
+    answers: {
+      a: "ActionScript",
+      b: "Java",
+      c: "ECMAScript"
     },
-    {
-      question: "Inna nazwa języka JavaScript to:",
-      answers: {
-        a: "ActionScript",
-        b: "Java",
-        c: "ECMAScript"
-      },
-      correctAnswer: "c"
-    }
-  ];
-
-  // show question
-  function showQuestion(i) {
-    let paragraph = document.querySelector('main p');
-    paragraph.textContent = questions[i].question;
-
+    correctAnswer: "c"
   }
+];
+// timer
+let myTimer;
+function startTimer() {
+  let minutes,
+      seconds,
+      duration = 300,
+      display = document.querySelector('#time'),
+      displayTimeOut = document.querySelector('#time-run-out');
 
-  // show next question
-  const nextButton = document.getElementById('next-btn');
-  nextButton.addEventListener("click", showNextQuestion);
-  var currentQuestion = 0;
-  
-  function showNextQuestion() {
-    showQuestion(currentQuestion + 1);
-    currentQuestion += 1;
-    if (currentQuestion === questions.length) {
-      nextButton.style.display = 'none';
+  function timer() {
+    duration --;
+    minutes = (duration / 60) | 0;
+    seconds = (duration % 60) | 0;
 
-    } else {
-      nextButton.style.display = 'block';
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = `Zostało ${minutes} : ${seconds} żeby zakończyć Quiz!`;
+
+    if (duration <= 0) {
+      clearInterval(myTimer);
+      displayTimeOut.textContent = 'Zakończył się czas';
     }
   }
 
-  function showResult() {
+  timer();
+  myTimer = clearInterval(myTimer);
+  myTimer = setInterval(timer, 1000);
+}
 
-    let correctAnswers = 3;
-    let titleText = 'Koniec Quizu!';
-    let contentText = `Odpowiedziałaś dobrze na ${correctAnswers} z ${questions.length} pytań.`;
+function stopTimer() {
+  clearInterval(myTimer);
+}
 
-    const mainElement = document.querySelector('main');
-    mainElement.innerHTML= `
+// show question
+function showQuestion(i) {
+  let paragraph = document.querySelector('main p');
+  paragraph.textContent = questions[i].question;
+  showAnswers(i);
+}
+
+//show answers
+function showAnswers(questionIndex) {
+  const q = questions[questionIndex];
+  const option1 = document.getElementById('answer1_label');
+  const option2 = document.getElementById('answer2_label');
+  const option3 = document.getElementById('answer3_label');
+  option1.innerHTML = `<input type = "radio" id = "answer1" name = "answer" value = "a" > </input> ${q.answers.a}`;
+  option2.innerHTML = `<input type = "radio" id = "answer2" name = "answer" value = "b" > </input> ${q.answers.b}`;
+  option3.innerHTML = `<input type = "radio" id = "answer3" name = "answer" value = "c" > </input> ${q.answers.c}`;
+}
+
+// show next question
+const nextButton = document.getElementById('next-btn');
+nextButton.addEventListener("click", showNextQuestion);
+let currentQuestion = 0;
+let correctAnswers = 0;
+
+const resultButton = document.getElementById('result-btn');
+resultButton.addEventListener("click", showResult);
+resultButton.style.display = 'none';
+
+
+function showNextQuestion() {
+  verify();
+  showQuestion(currentQuestion + 1);
+  currentQuestion += 1;
+  if (currentQuestion === questions.length - 1) {
+    nextButton.style.display = 'none';
+
+    resultButton.style.display = 'block';
+    showResult();
+    stopTimer();
+  } else {
+    nextButton.style.display = 'block';
+  }
+
+}
+
+// verify currentQuestion. if it's correct increase correctAnswers counter
+function verify() {
+  let currentAnswer = document.querySelector('input[name=answer]:checked').value;
+  if (currentAnswer && questions[currentQuestion].correctAnswer === currentAnswer) {
+    correctAnswers++;
+  }
+}
+
+function showResult() {
+
+  let titleText = 'Koniec Quizu!';
+  let contentText = `Odpowiedziałaś dobrze na ${correctAnswers} z ${questions.length} pytań.`;
+
+  const mainElement = document.querySelector('main');
+  mainElement.innerHTML = `
       <p>${titleText}</p>
       <p>${contentText}</p>
+      <button id="last-btn" class="btn">Jeszcze raz</button>
     `;
-  }
+  const lastButton = document.getElementById('last-btn');
+  lastButton.addEventListener('click', startQuiz);
+
+}
+
+function startQuiz() {
+  const mainElement = document.querySelector('main');
+  mainElement.innerHTML = `
+  <p>treść pytania</p>
+    <radiogroup class="answers">   
+      <label id="answer1_label"><input type="radio" id="answer1" name="answer" value="a"/>odpowiedź pierwsza</label>
+      <label id="answer2_label"><input type="radio" id="answer2" name="answer" value="b"/>odpowiedź druga</label>
+      <label id="answer3_label"><input type="radio" id="answer3" name="answer" value="c"/>odpowiedź trzecia</label>      
+    </radiogroup>
+    <button id="next-btn" class="btn">Dalej</button>
+    `;
+  const nextButton = document.getElementById('next-btn');
+  nextButton.addEventListener("click", showNextQuestion);
+
+  currentQuestion = 0;
+  showQuestion(currentQuestion);
+  startTimer();
+
+}
+
+startQuiz();
+
+
 
   //INFORMATION PANEL
 let step = 'step1';
